6a628ee8c6145e36c4b2a8536bf959ac32b6c50f
shin seung woo <ssw030209@gmail.com>
2025-04-24 17:36:52 +0900

    first commit

---
+++ src/main/java/com/example/book/controller/BookController.java
@@ -0,0 +1,19 @@
+package com.example.book.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    @GetMapping("/list")
+    public void getList() {
+
+    }
+}
