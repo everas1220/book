07ab5b380125521a26410f6151ca653c2ebabeac
shin seung woo <ssw030209@gmail.com>
2025-04-24 18:04:57 +0900

    추가 수정

--- src/main/java/com/example/book/controller/BookController.java
+++ src/main/java/com/example/book/controller/BookController.java
@@ -3,7 +3,6 @@ package com.example.book.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.book.dto.BookDTO;
 
