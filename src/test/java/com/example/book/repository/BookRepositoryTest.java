006ded058b8d8df5d88222d80db67a57994527e7
shin seung woo <ssw030209@gmail.com>
2025-05-07 18:06:13 +0900

    2025-05-07

--- src/test/java/com/example/book/repository/BookRepositoryTest.java
+++ src/test/java/com/example/book/repository/BookRepositoryTest.java
@@ -1,5 +1,4 @@
 package com.example.book.repository;
-
 import java.util.stream.IntStream;
 
 import org.junit.jupiter.api.Test;
@@ -11,6 +10,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import com.example.book.entity.Book;
+import com.example.book.repository.BookRepository;
 
 @SpringBootTest
 public class BookRepositoryTest {
@@ -38,14 +38,14 @@ public class BookRepositoryTest {
     }
 
     @Test
-    public void testList2() {
+    public void testList1() {
         // 페이지 나누기
-        Pageable pageable = PageRequest.of(1, 10, Sort.by("code").descending());
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("code").descending());
 
-        Page<Book> result = bookRepository.findAll(pageable);
+        Page<Book> result = bookRepository.findAll(pageable)
         result.getContent().forEach(book -> System.out.println(book));
-        System.out.println("전체 행 개수 " + result.getTotalElements());
-        System.out.println("전체 페이지 수 " + result.getTotalPages());
+        System.out.println("전체 행 갯수" + result.getTotalElements());
+        System.out.println("전체 페이지 수" + result.getTotalPages());
     }
 
     @Test
